cfd51168ee3221f1b180318a67b02bad59e83dc7
joeyfar624 <160137728+joeyfar624@users.noreply.github.com>
2024-02-20 09:30:34 -0500

    Add files via upload

---
+++ fooditems.js
@@ -0,0 +1,121 @@
+const foodItem = [
+    {
+        id: 1,
+        name: 'BLT Sandwich',
+        category: 'classic',
+        rating: 4.7,
+        price: 11,
+        img: 'bltSandwich.png',
+        quantity: 1
+    },
+    {
+        id: 2,
+        name: 'Cheese Burger',
+        category: 'classic',
+        rating: 4.6,
+        price: 15,
+        img: 'burg.png',
+        quantity: 1
+    },
+    {
+        id: 3,
+        name: 'pasta',
+        category: 'classic',
+        rating: 4.2,
+        price: 18,
+        img: 'pasta.jpg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        name: 'BBQ Ribs',
+        category: 'classic',
+        rating: 4.9,
+        price: 15,
+        img: 'ribs.jpg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        name: 'Fried Shrimp',
+        category: 'appetizer',
+        rating: 4.1,
+        price: 10,
+        img: 'fShrimp.png',
+        quantity: 1
+    },
+    {
+        id: 6,
+        name: 'Bavarian Pretzels',
+        category: 'appetizer',
+        rating: 4.3,
+        price: 12,
+        img: 'pretzel.jpg',
+        quantity: 1
+    },
+    {
+        id: 7,
+        name: 'wings',
+        category: 'classic',
+        rating: 3.9,
+        price: 13,
+        img: 'wings.png',
+        quantity: 1
+    },
+    {
+        id: 8,
+        name: 'Steak',
+        category: 'classic',
+        rating: 4.4,
+        price: 20,
+        img: 'steak.png',
+        quantity: 1
+    },
+    {
+        id: 9,
+        name: 'salad',
+        category: 'appetizer',
+        rating: 4.0,
+        price: 15,
+        img: 'salad.jpg',
+        quantity: 1
+    },
+    {
+        id: 10,
+        name: 'Waffle Fries',
+        category: 'side',
+        rating: 3.8,
+        price: 11,
+        img: 'fries.jpg',
+        quantity: 1
+    },
+    {
+        id: 11,
+        name: 'Onion Rings',
+        category: 'side',
+        rating: 4.2,
+        price: 10,
+        img: 'oRings.jpg',
+        quantity: 1
+    },
+    {
+        id: 12,
+        name: 'potato Wedges',
+        category: 'side',
+        rating: 4.6,
+        price: 14,
+        img: 'potatoWedges.jpg',
+        quantity: 1
+    },
+    {
+        id: 13,
+        name: 'Strawberry Shortcake',
+        category: 'dessert',
+        rating: 4.8,
+        price: 12,
+        img: 'strawberry.jpg',
+        quantity: 1
+    },
+]
+
+export { foodItem };
